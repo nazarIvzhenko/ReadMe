836106868112d29420e7cbd3b6ac48b0d4648fbb
nazarIvzhenko <nazarivzhenko@icloud.com>
2023-01-27 09:54:58 +0200

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Nazar Ivzhenko on 27.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
